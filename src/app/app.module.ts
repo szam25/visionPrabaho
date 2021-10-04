8029eff51502c7ce31045aed509e44b4254c9f34
Souvik <s.zaminder@gmail.com>
2021-10-04 17:55:19 +0530

    Work on 04/10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { VisionComponent } from './vision/vision.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    VisionComponent,
   ],
   imports: [
     BrowserModule,
