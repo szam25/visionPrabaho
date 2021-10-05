dc756f158afdf88c33bca444db6e7bf77d967ff4
Souvik <s.zaminder@gmail.com>
2021-10-05 15:07:16 +0530

    work_on_05/10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { VisionComponent } from './vision/vision.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'aboutUS', component: VisionComponent},
+  {path: '', component: HomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
