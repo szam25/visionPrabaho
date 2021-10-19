bd053ea0481c5484e885178c69a6b2497dd7b05e
Souvik <s.zaminder@gmail.com>
2021-10-19 19:22:10 +0530

    work on 19/10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { GetInvolvedComponent } from './get-involved/get-involved.component';
 import { HomeComponent } from './home/home.component';
 import { VisionComponent } from './vision/vision.component';
 
 const routes: Routes = [
   {path: 'aboutUS', component: VisionComponent},
+  {path: 'getInvolved', component: GetInvolvedComponent},
   {path: '', component: HomeComponent}
 ];
 
